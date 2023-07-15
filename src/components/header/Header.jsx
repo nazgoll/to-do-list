066be743808fd783b2d16f07982b7a61105448c1
nazgol <nazgool.momeni1@gmail.com>
2023-07-15 12:05:23 +0330

    add header component

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+const Header = ({children})=> {
+    return (
+        <div>
+            <header className="text-blue-900 text-3xl text-center
+             pb-7 font-bold">
+                To Do List
+            </header>
+        </div>
+    )
+}
+
+
+
+export default Header;
